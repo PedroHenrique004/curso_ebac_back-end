ca58fd40c873065f5abf49a6b7a54b3b93bbc263
Pedro Santos <pedrohenriqueph004@gmail.com>
2023-10-26 21:47:45 -0300

    Exercicio do módulo 9

---
+++ Exercicios/src/ExercicioM9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ExercicioM9 {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        System.out.println(x);
+
+        short y = (short) x;
+        System.out.println(y);
+    }
+}
