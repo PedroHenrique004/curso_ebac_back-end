e4288897fc5fe2a32df2af4a8d3509c949577e73
Pedro Santos <pedrohenriqueph004@gmail.com>
2023-10-29 23:22:02 -0300

    Exercicio do módulo 11

---
+++ Exercicios/src/ExercicioMod11/Individuos.java
@@ -0,0 +1,28 @@
+package ExercicioMod11;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Individuos {
+
+
+    public String nome;
+
+    public String sexo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
---
+++ Exercicios/src/ExercicioMod11/MetodosDoexercicio.java
@@ -0,0 +1,33 @@
+package ExercicioMod11;
+
+import java.util.Scanner;
+
+public class MetodosDoexercicio {
+
+    public static void main(String args[]){
+
+        lerNomeSexo();
+    }
+
+    public static void lerNomeSexo() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nome da 1 pessoa: ");
+        nome = sc.nextLine();
+
+        System.out.println("Sexo da 1 pessoa: ");
+        sexo = sc.nextLine();
+
+        System.out.println("Nome da 2 pessoa: ");
+        nome = sc.nextLine();
+
+        System.out.println("Sexo da 2 pessoa: ");
+        sexo = sc.nextLine();
+
+        System.out.println("Nome da 3 pessoa: ");
+        nome = sc.nextLine();
+
+        System.out.println("Sexo da 3 pessoa: ");
+        sexo = sc.nextLine();
+    }
+}
